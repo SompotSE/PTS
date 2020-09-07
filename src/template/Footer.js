247df1ea098d4678a066c3235eec3d041285d518
SompotSE <SompotSE>
2020-09-08 00:40:42 +0700

    first commit

---
+++ src/template/Footer.js
@@ -0,0 +1,18 @@
+import React, { Component } from "react";
+
+import '../css/footer.css';
+
+export default class Footer extends Component {
+    render() {
+        return (
+            <div >
+                <div id="footer-bule">
+                     
+                </div>
+                <div id="footer-black">
+
+                </div>
+            </div>
+        )
+    }
+}
