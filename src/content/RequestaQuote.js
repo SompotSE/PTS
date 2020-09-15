87cc3546966a92e81b01850f6e05f2f7aaaa1af0
unknown <s5902041620113@email.kmutnb.ac.th>
2020-09-15 21:35:19 +0700

    RequestaQuote

---
+++ src/content/RequestaQuote.js
@@ -0,0 +1,56 @@
+import React, { Component } from "react";
+import { Form, Container, Row, Button, Col, Column, } from 'react-bootstrap';
+
+const sty = {
+
+}
+
+export default class Request extends Component {
+    render() {
+        return (
+            <Container>
+                <Row style={{marginTop:100}}>
+                    <Col xs={1} md={1}></Col>
+                    <Col xs={10} md={10} style={{ border: "groove", padding: "5%"}}>
+                        <div style={{ textAlign: 'center', fontSize: '35', fontWeight: 'bold', paddingBottom: '3%'}}>REQUEST A QUOTE</div>
+                        <div>Please fill out as much of the information below as possible. If you areunsure of your testing requirements, we will follow up anyway to help
+clarify. You also welcome to call us at +1979 690 2751 x2 (for sales) to discuss.</div>
+                        <Form>
+                            <Form.Group controlId="formGroupName">
+                                <Form.Label>Name*</Form.Label>
+                                <Form.Control type="name" placeholder="First & Last (Family) Name" />
+                            </Form.Group>
+                            <Form.Group controlId="formGroupCompany">
+                                <Form.Label>Company</Form.Label>
+                                <Form.Control type="company" />
+                            </Form.Group>
+                            <Form.Group controlId="formGroupContry">
+                                <Form.Label>Contry*</Form.Label>
+                                <Form.Control type="contry" placeholder="Please list the contry where you are inquiring." />
+                            </Form.Group>
+                            <Form.Group controlId="formGroupEmail">
+                                <Form.Label>Email*</Form.Label>
+                                <Form.Control type="email" />
+                            </Form.Group>
+                            <Form.Group controlId="formGroupPhone">
+                                <Form.Label>Phone*</Form.Label>
+                                <Form.Control type="phone" placeholder="Please list the contry where you are inquiring." />
+                            </Form.Group>
+                            <Form.Group controlId="formGridAddress">
+                                <Form.Label>Address</Form.Label>
+                                <Form.Control type="address" placeholder="Please enter your company adress" as="textarea" rows="5"/>
+                            </Form.Group>
+                            <Form.Group controlId="formGridDescribe">
+                                <Form.Label>Please describe your testing application.</Form.Label>
+                                <Form.Control type="describe" placeholder="What device(s) are being tested; what type of tests will be performed , etc." as="textarea" rows="5"/>
+                            </Form.Group>
+                            <Button type="submit">SUBMIT</Button>
+                        </Form>
+                    </Col>
+                    <Col xs={1} md={1}></Col>
+                </Row>
+            </Container>
+        )
+
+    }
+}
---
+++ src/content/RequestaQuote1.js
@@ -0,0 +1,68 @@
+import React, { Component } from "react";
+import { Form, Container, Row, Button, Col, Image } from 'react-bootstrap';
+// import map from '../image/map.png'
+import map from '../image/map.png';
+
+const sty = {
+
+}
+
+export default class Request extends Component {
+    render() {
+        return (
+            <Container>
+                <Row style={{marginTop:'10%', marginBottom: '5%' }}>
+                    <Col xs={1} md={1}></Col>
+                    <Col xs={10} md={10}>
+                        {/* <Image src={map.png} fluid responsive /> */}
+                        <Image src={map} fluid responsive />
+                    </Col>
+                    <Col xs={1} md={1}></Col>
+                </Row>
+                <Row style={{}}>
+                    <Col xs={1} md={1}></Col>
+                    <Col xs={6} md={6}>
+                        <div style={{ textAlign: 'left', fontSize: '30', fontWeight: 'bold', paddingBottom: '3%',color:'DarkBlue'}}>CONTRACT US</div>
+                        <Form>
+                            <Form.Group controlId="formGroupName">
+                                <Form.Label>Name</Form.Label>
+                                <Form.Control type="name" as="textarea" rows="2"/>
+                            </Form.Group>
+                            <Form.Group controlId="formGroupEmail">
+                                <Form.Label>Email</Form.Label>
+                                <Form.Control type="email" as="textarea" rows="2"/>
+                            </Form.Group>
+                            <Form.Group controlId="formGroupPhone">
+                                <Form.Label>Phone</Form.Label>
+                                <Form.Control type="phone" as="textarea" rows="2" />
+                            </Form.Group>
+                            <Form.Group controlId="formGroupMessage">
+                                <Form.Label>Message</Form.Label>
+                                <Form.Control type="message" placeholder="Write message" as="textarea" rows="5" />
+                            </Form.Group>
+                            <Form.Text id="passwordHelpBlock" muted>
+                                Please enter the result number from above.
+                            </Form.Text>
+                            <Button type="submit" style={{background: 'Navy', marginTop:'1%'}}>Send message</Button>
+                        </Form>
+                    </Col>
+                    <Col xs={1} md={1}></Col>
+                    <Col xs={3} md={3}>
+                        <div style={{ textAlign: 'left', fontSize: '30', fontWeight: 'bold', paddingTop: '25%'}}>ADDRESS</div>
+                        <div>PTS COMBINATION CO., LTD. 11/26 Moo 1, Banmai tumbol, Pakkret, Nontaburi, 11120 Thailand</div>
+                        <div></div>
+                        <div style={{ textAlign: 'left', fontSize: '30', fontWeight: 'bold', paddingTop: '15%'}}>Contract :</div>
+                        <div>Tel. 02-5015677, 02-5015228</div>
+                        <div>Fax. 02-5015357</div>
+                        <div>Email pts.sales8@gmail.com</div>
+                        <div style={{ textAlign: 'left', fontSize: '30', fontWeight: 'bold', paddingTop: '15%'}}>Office Hours</div>
+                        <div>M0-Fr : 8.30 - 17.00</div>
+                        <div>Sa-Su : closed</div>
+                    </Col>
+                    <Col xs={1} md={1}></Col>
+                </Row>
+            </Container>
+        )
+
+    }
+}
