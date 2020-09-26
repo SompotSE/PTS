a2c31b9820ef2af4cbce939a2ea9bf159da61d99
SompotSE <SompotSE>
2020-09-26 22:58:00 +0700

    iphone

--- src/template/LiveChat.js
+++ src/template/LiveChat.js
@@ -275,20 +275,20 @@ export default class Footer extends Component {
             >
                 <PopoverBody>
                     <Row id="row-popup">
-                        <Col md={6} xs={8} style={textLiveChat}>Line Chat</Col>
-                        <Col md={6} xs={4} style={iconFooter}><Image src={logoLine} id="Popover-top" style={{ width: "40%" }} fluid responsive /></Col>
+                        <Col md={6} xs={6} style={textLiveChat}>Line Chat</Col>
+                        <Col md={6} xs={6} style={iconFooter}><Image src={logoLine} id="Popover-top" style={{ width: "40%" }} fluid responsive /></Col>
                     </Row>
                     <Row id="row-popup">
-                        <Col md={6} xs={8} style={textLiveChat}>Tel. Phone</Col>
-                        <Col md={6} xs={4} style={iconFooter}>
+                        <Col md={6} xs={6} style={textLiveChat}>Tel. Phone</Col>
+                        <Col md={6} xs={6} style={iconFooter}>
                             <a href="tel:+66831885535">
                                 <Image src={logoPhone} id="Popover-top" style={{ width: "40%" }} fluid responsive />
                             </a>
                         </Col>
                     </Row>
                     <Row id="row-popup">
-                        <Col md={6} xs={8} style={textLiveChat}>Request</Col>
-                        <Col md={6} xs={4} style={iconFooter}>
+                        <Col md={6} xs={6} style={textLiveChat}>Request</Col>
+                        <Col md={6} xs={6} style={iconFooter}>
                             <NavLink to="/Request">
                                 <Image src={logoKa} id="Popover-top" style={{ width: "40%" }} fluid responsive />
                             </NavLink>
