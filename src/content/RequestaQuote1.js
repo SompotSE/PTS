5f56a4ea7f5c63f12cdb37fff1c4415f0aad3747
SompotSE <SompotSE>
2020-11-22 23:28:37 +0700

    Save Contact

--- src/content/RequestaQuote1.js
+++ src/content/RequestaQuote1.js
@@ -2,8 +2,52 @@ import React, { Component } from "react";
 import { Form, Container, Row, Button, Col, Image } from 'react-bootstrap';
 import map from '../image/map.png';
 // import GoogleMap from './RequestaQuote1Map';
+var ip = "http://178.128.209.69";
 
 class Request extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            name: "",
+            email: "",
+            phone: "",
+            message: ""
+        };
+
+        this.onSubmit = this.onSubmit.bind(this);
+    }
+
+    async onSubmit() {
+        console.log(this.state.name, " this.state.name");
+        console.log(this.state.email, " this.state.email");
+        console.log(this.state.phone, " this.state.phone");
+        console.log(this.state.message, " this.state.message");
+        if (this.state.name === "" || this.state.email === "" || this.state.phone === "" || this.state.message === "") {
+            alert("Please complete all information.");
+            return false;
+        }  else {
+            const save_contact = {
+                name: this.state.name,
+                email: this.state.email,
+                phone: this.state.phone,
+                message: this.state.message
+            }
+
+            const response = await fetch(ip + '/PTS/SaveContact.php', {
+                method: 'POST',
+                body: JSON.stringify(save_contact)
+            });
+
+            const res = await response.json();
+            if (res.code === 200) { 
+                alert("Save Data Successfully.");
+                window.location.replace('/Contact', false);
+            } else {
+                alert("Save Data Error");
+            }
+        }
+    }
+
     render() {
         return (
             <Container>
@@ -30,21 +74,45 @@ class Request extends Component {
                         <Form>
                             <Form.Group controlId="formGroupName">
                                 <Form.Label>Name</Form.Label>
-                                <Form.Control type="name" as="textarea" rows="2" />
+                                <Form.Control 
+                                    type="name" 
+                                    as="textarea" 
+                                    rows="2" 
+                                    value={this.state.name}
+                                    onChange={e => this.setState({ name: e.target.value })}
+                                />
                             </Form.Group>
                             <Form.Group controlId="formGroupEmail">
                                 <Form.Label>Email</Form.Label>
-                                <Form.Control type="email" as="textarea" rows="2" />
+                                <Form.Control 
+                                    type="email" 
+                                    as="textarea" 
+                                    rows="2" 
+                                    value={this.state.email}
+                                    onChange={e => this.setState({ email: e.target.value })}
+                                />
                             </Form.Group>
                             <Form.Group controlId="formGroupPhone">
                                 <Form.Label>Phone</Form.Label>
-                                <Form.Control type="phone" as="textarea" rows="2" />
+                                <Form.Control 
+                                    type="phone" 
+                                    as="textarea" 
+                                    rows="2" 
+                                    value={this.state.phone}
+                                    onChange={e => this.setState({ phone: e.target.value })}
+                                />
                             </Form.Group>
                             <Form.Group controlId="formGroupMessage">
                                 <Form.Label>Message</Form.Label>
-                                <Form.Control type="message" placeholder="Write message" as="textarea" rows="5" />
+                                <Form.Control 
+                                    type="message" 
+                                    placeholder="Write message" 
+                                    as="textarea" rows="5" 
+                                    value={this.state.message}
+                                    onChange={e => this.setState({ message: e.target.value })}
+                                />
                             </Form.Group>
-                            <Button type="submit" style={{ background: 'Navy', marginTop: '1%' }}>Send message</Button>
+                            <Button style={{ background: 'Navy', marginTop: '1%' }} onClick={this.onSubmit}>Send message</Button>
                         </Form>
                     </Col>
                     <Col xs={2} md={1}></Col>
