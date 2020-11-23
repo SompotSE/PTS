db40d68e03d7736b4868254355d6bffde3244212
SompotSE <SompotSE>
2020-11-23 20:15:26 +0700

    onClick go to google map

--- src/content/RequestaQuote1Map.js
+++ src/content/RequestaQuote1Map.js
@@ -5,10 +5,10 @@ import '../css/marker.css';
 
 // const AnyReactComponent = ({ text }) => <div>{text}</div>;
 
-const AnyReactComponent = ({ text }) => 
-<>
-    <div className="pin"></div> <div className="pulse"></div>
-</>;
+const AnyReactComponent = ({ text }) =>
+    <>
+        <div className="pin"></div> <div className="pulse"></div>
+    </>;
 
 class RequestaQuote1Map extends Component {
     static defaultProps = {
@@ -28,6 +28,8 @@ class RequestaQuote1Map extends Component {
                     // bootstrapURLKeys={{ key: "" }}
                     defaultCenter={this.props.center}
                     defaultZoom={this.props.zoom}
+                    yesIWantToUseGoogleMapApiInternals
+                    onClick={() => { window.open("https://www.google.com/maps/place/13%C2%B056'37.4%22N+100%C2%B033'49.6%22E/@13.9437202,100.5615853,17z/data=!3m1!4b1!4m5!3m4!1s0x0:0x0!8m2!3d13.943715!4d100.563774", "_blank"); }}
                 >
                     {/* <OverlayTrigger
                         trigger="click"
@@ -39,11 +41,11 @@ class RequestaQuote1Map extends Component {
                             </Tooltip>
                         }
                     > */}
-                        <AnyReactComponent
-                            lat={13.943715}
-                            lng={100.563774}
-                            text="PTS COMBINATION"
-                        />
+                    <AnyReactComponent
+                        lat={13.943715}
+                        lng={100.563774}
+                        text="PTS COMBINATION"
+                    />
                     {/* </OverlayTrigger> */}
                 </GoogleMapReact>
             </div >
